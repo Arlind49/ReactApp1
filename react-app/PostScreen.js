eb3db83d3cd2e0b65af8a2e81f1101683c5349ad
Arlind Selimi <68119170+Arlind49@users.noreply.github.com>
2024-11-04 17:38:53 +0100

    Create PostScreen.js

---
+++ react-app/PostScreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Text,StyleSheet ,View ,Flatlist} from "react-native";
+
+
+    class PostScreen extends React.Component{
+        constructor(){
+            super();
+            this.state={
+                posts: []
+
+
+            }
+        }
+
+        async componentDidMount(){
+            const data = await fetch("https://jsonplaceholder.typicode.com/posts")
+            const jsonData = await data.json();
+            this.setState({posts: jsonData})
+        }   
+
+        render(){
+            const {posts} = this.state
+            return(
+                <View>
+                    <Text>Post</Text>
+                    <Flatlist keyExtractor={posts => posts.id}
+                    data={posts} renderItem={({item}) =>{
+                        <View>
+                            <Text>{item.id}</Text>
+                            <Text>{item.title}</Text>
+                        </View>
+
+                    }}
+
+                    ></Flatlist>
+                </View>
+            )
+        }
+
+    }
+
+const styles = StyleSheet.create({});
+
+export default PostScreen;
